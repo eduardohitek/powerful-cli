bddcc1c240c49361d85c8c2ba9ec56329b34bb0c
Eduardo Hitek <eduardohitek@gmail.com>
2022-11-20 07:55:48 -0300

    feat: bash para geracao automática

--- chapter3/mdp/README.md
+++ chapter3/mdp/README.md
@@ -1,7 +1,6 @@
 # Example Markdown File
 
 This is an example Markdown file to test the preview tool
-
 ## Features:
 * Support for links [PragProg](https://pragprog.com)
 * Support for other features
--- chapter3/mdp/main.go
+++ chapter3/mdp/main.go
@@ -8,6 +8,7 @@ import (
 	"os"
 	"os/exec"
 	"runtime"
+	"time"
 
 	"github.com/microcosm-cc/bluemonday"
 	"github.com/russross/blackfriday/v2"
@@ -69,6 +70,8 @@ func run(filename string, out io.Writer, skipPreview bool) error {
 		return nil
 	}
 
+	defer os.Remove(outName)
+
 	return preview(outName)
 }
 
@@ -114,5 +117,8 @@ func preview(name string) error {
 		return err
 	}
 
-	return exec.Command(cPath, cParams...).Run()
+	err = exec.Command(cPath, cParams...).Run()
+	time.Sleep(2 * time.Second)
+
+	return err
 }
