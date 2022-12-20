3f9863d3113193aafe958152249223b0197a1c92
Eduardo Hitek <eduardohitek@gmail.com>
2022-12-20 11:57:13 -0300

    feat: organizando os passos em pipeline

--- chapter6/goci/main.go
+++ chapter6/goci/main.go
@@ -5,24 +5,32 @@ import (
 	"fmt"
 	"io"
 	"os"
-	"os/exec"
 )
 
 func run(proj string, out io.Writer) error {
 	if proj == "" {
 		return fmt.Errorf("project directory is required: %w", ErrValidation)
 	}
-	args := []string{"build", ".", "errors"}
-	cmd := exec.Command("go", args...)
-	cmd.Dir = proj
+	pipeline := make([]step, 1)
+	pipeline[0] = newStep(
+		"go build",
+		"go",
+		"Go Build: SUCCESS",
+		proj,
+		[]string{"build", ".", "errors"},
+	)
 
-	err := cmd.Run()
-	if err != nil {
-		return &stepErr{step: "go build", msg: "go build failed", cause: err}
+	for _, p := range pipeline {
+		msg, err := p.execute()
+		if err != nil {
+			return err
+		}
+		_, err = fmt.Fprintln(out, msg)
+		if err != nil {
+			return err
+		}
 	}
-
-	_, err = fmt.Fprintln(out, "Go Build: SUCCESS")
-	return err
+	return nil
 }
 
 func main() {
